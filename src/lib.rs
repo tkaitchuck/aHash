5a95d6db8d217371b324ad3bf828f2a8239722d4
Tom <Tom.Kaitchuck@dell.com>
2019-02-28 23:24:13 -0800

    Add more docs and fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,20 @@
+//! # aHash
+//!
+//! This hashing algorithm is intended to be a high perfromance, (hardware specific), keyed hash function.
+//! This can be seen as a DOS resistant alternative to FxHash, or a fast equivalent to SipHash.
+//! It provides a high speed hash algorithm, but where the result is not predictable without knowing a Key.
+//! This allows it to be used in a HashMap without allowing for the possibility that an malicious user can
+//! induce a collision.
+//!
+//! # How aHash works
+//!
+//! aHash uses the hardware AES instruction on x86 processors to provide a keyed hash function.
+//! It uses two rounds of AES per hash. So it should not be considered cryptographically secure.
+
 mod convert;
 
-///# aHash
-///
-/// This hashing algorithm is intended to be a DOS resistant, hardware specific, Hash.
-/// This can be seen as a DOS resistant alternative to FxHash, or a fast equivalent to SipHash.
-/// It provides a high speed hash algorithm, and is a Keyed hash. This allows it to be used
-/// in a HashMap without allowing for the possibility that an malicious user can induce a collision.
-///
-/// # How aHash works
-///
-/// aHash uses the hardware AES instruction on x86 processors to provide a keyed hash function.
-/// It uses two rounds of AES per hash. So it should not be considered cryptographically secure.
-
 use crate::convert::Convert;
-use std::collections::{HashMap};
+use std::collections::HashMap;
 use std::default::Default;
 use std::hash::{BuildHasherDefault, Hasher, BuildHasher};
 use std::mem::transmute;
@@ -23,16 +24,16 @@ pub type AHashMap<K, V> = HashMap<K, V, LocationBasedState>;
 
 const DEFAULT_KEYS: [u64; 2] = [0x6c62_272e_07bb_0142, 0x517c_c1b7_2722_0a95];
 
-/// Provides a `BuildHasher` is typically used (e.g. by [`HashMap`]) to create
-/// [`AHasher`]s for each key such that they are hashed independently of one
-/// another, since [`AHasher`]s contain state.
+/// Provides a [BuildHasher] is typically used (e.g. by [HashMap]) to create
+/// [AHasher]s for each key such that they are hashed independently of one
+/// another, since [AHasher]s contain state.
 ///
 /// LocationBasedState uses it's in-memory location to see the AHasher. This
 /// is more predictable than true random values, but does not require generating
-/// them.
+/// randomness.
 ///
-/// For each instance of `LocationBasedState`, the [`AHasher`]s created by
-/// [`build_hasher`] should be identical. That is, if the same stream of bytes
+/// For each instance of [LocationBasedState], the [AHasher]s created by
+/// [BuildHasher] should be identical. That is, if the same stream of bytes
 /// is fed into each hasher, the same output will also be generated.
 ///
 /// # Examples
@@ -50,34 +51,34 @@ const DEFAULT_KEYS: [u64; 2] = [0x6c62_272e_07bb_0142, 0x517c_c1b7_2722_0a95];
 ///
 /// assert_eq!(hasher_1.finish(), hasher_2.finish());
 /// ```
-///
-/// [`build_hasher`]: #tymethod.build_hasher
-/// [`Hasher`]: trait.Hasher.html
-/// [`HashMap`]: ../../std/collections/struct.HashMap.html
-pub struct LocationBasedState { }
+/// [BuildHasher]: std::hash::BuildHasher
+/// [Hasher]: std::hash::Hasher
+pub struct LocationBasedState {}
 
 impl LocationBasedState {
     pub fn new() -> LocationBasedState {
-        LocationBasedState{}
+        LocationBasedState {}
     }
 }
+
 impl BuildHasher for LocationBasedState {
     type Hasher = AHasher;
 
     fn build_hasher(&self) -> AHasher {
         let location = self as *const Self as u64;
-        AHasher {buffer:[location, location.rotate_left(8)]}
+        AHasher { buffer: [location, location.rotate_left(8)] }
     }
 }
+
 impl Default for LocationBasedState {
     fn default() -> LocationBasedState {
-        LocationBasedState{}
+        LocationBasedState {}
     }
 }
 
 /// A `Hasher` for hashing an arbitrary stream of bytes.
 ///
-/// Instances of `AHasher` represent state that is updated while hashing data.
+/// Instances of [AHasher] represent state that is updated while hashing data.
 ///
 /// Each method updates the internal state based on the new data provided. Once
 /// all of the data has been provided, the resulting hash can be obtained by calling
@@ -98,15 +99,22 @@ impl Default for LocationBasedState {
 ///
 /// println!("Hash is {:x}!", hasher.finish());
 /// ```
+/// [Clone] is also provided in case you wish to calculate hashes for two different items that
+/// start with the same data.
+///
 #[derive(Debug, Clone)]
 pub struct AHasher {
     buffer: [u64; 2],
 }
+
 impl AHasher {
     pub fn new_with_keys(key0: u64, key1: u64) -> AHasher {
-        AHasher { buffer:[key0, key1] }
+        AHasher { buffer: [key0, key1] }
     }
 }
+
+/// Constructs a new [AHasher] with fixed constant keys.
+/// (This is not recommended in situations where a DOS attack is a concern)
 impl Default for AHasher {
     #[inline]
     fn default() -> AHasher {
@@ -129,29 +137,28 @@ macro_rules! as_array {
     }}
 }
 
-//Implementation note: each of the write_XX methods passes the arguments slightly differently to hash.
-//This is done so that an u8 and a u64 that both contain the same value will produce different hashes.
+/// Provides methods to hash all of the primitive types.
 impl Hasher for AHasher {
+    //Implementation note: each of the write_XX methods passes the arguments slightly differently to hash.
+    //This is done so that an u8 and a u64 that both contain the same value will produce different hashes.
     #[inline]
     fn write_u8(&mut self, i: u8) {
         self.buffer = hash([self.buffer[1], self.buffer[0] ^ i as u64].convert(), self.buffer.convert()).convert();
-
     }
 
     #[inline]
     fn write_u16(&mut self, i: u16) {
         self.buffer = hash([self.buffer[1] ^ i as u64, self.buffer[0]].convert(), self.buffer.convert()).convert();
-
     }
 
     #[inline]
     fn write_u32(&mut self, i: u32) {
-        self.buffer = hash([self.buffer[0], self.buffer[1]  ^ i as u64].convert(), self.buffer.convert()).convert();
+        self.buffer = hash([self.buffer[0], self.buffer[1] ^ i as u64].convert(), self.buffer.convert()).convert();
     }
 
     #[inline]
     fn write_u128(&mut self, i: u128) {
-        let buffer: u128 = self.buffer.convert(); 
+        let buffer: u128 = self.buffer.convert();
         self.buffer = hash((buffer ^ i).convert(), self.buffer.convert()).convert();
     }
 
@@ -225,8 +232,8 @@ fn hash(value: [u8; 16], xor: [u8; 16]) -> [u8; 16] {
 }
 
 #[cfg(all(
-    any(target_arch = "x86", target_arch = "x86_64"),
-    target_feature = "aes"
+any(target_arch = "x86", target_arch = "x86_64"),
+target_feature = "aes"
 ))]
 #[cfg(test)]
 mod tests {
@@ -266,11 +273,11 @@ mod tests {
         let result2: [u8; 16] = result2.convert();
         assert_ne!(hex::encode(result), hex::encode(result2));
     }
+
     #[test]
     fn test_conversion() {
         let input: &[u8] = "dddddddd".as_bytes();
         let bytes: u64 = as_array!(input, 8).convert();
         assert_eq!(bytes, 0x6464646464646464);
     }
-
 }
