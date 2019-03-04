616adfc109f6d4cc2cff19f8f593c12f0be6bd89
Tom <Tom.Kaitchuck@dell.com>
2019-03-03 16:50:57 -0800

    Improve strength of tests

--- tests/crypto.rs
+++ tests/crypto.rs
@@ -1,24 +1,17 @@
 use ahash::*;
 use std::hash::{Hash, Hasher};
 
-///Basic sanity tests of the cypto properties of aHash. 
+const BAD_KEY: u64 = 0x0123456789ABCDEF;
+
+///Basic sanity tests of the cypto properties of aHash.
 #[cfg(test)]
 mod crypto {
     use crate::*;
 
     pub fn ahash<H: Hash>(b: H) -> u64 {
-        let mut hasher = AHasher::new_with_keys(0, 0);
+        let mut hasher = AHasher::new_with_keys(BAD_KEY, BAD_KEY);
         b.hash(&mut hasher);
         hasher.finish()
-    }
-
-    fn assert_sufficently_different(a: u64, b: u64) {
-        let flipped_bits = (a ^ b).count_ones();
-        assert!(flipped_bits > 14 && flipped_bits < 50, "{:x} and {:x}: {:}", a, b, flipped_bits);
-        for rotate in 0..64 {
-            let flipped_bits = (a ^ (b.rotate_left(rotate))).count_ones();
-            assert!(flipped_bits > 10 && flipped_bits < 54, "{:x} and {:x}: {:x}", a, b, flipped_bits);
-        }
     }
 
     #[test]
@@ -27,19 +20,19 @@ mod crypto {
         let compare_value = ahash(0u32);
         for pos in 0..size {
             let test_value = ahash(0 ^ (1u32 << pos));
-            assert_sufficently_different(compare_value, test_value);
+            assert_sufficiently_different(compare_value, test_value);
         }
         let size = 64;
         let compare_value = ahash(0u64);
         for pos in 0..size {
             let test_value = ahash(0 ^ (1u64 << pos));
-            assert_sufficently_different(compare_value, test_value);
+            assert_sufficiently_different(compare_value, test_value);
         }
         let size = 128;
         let compare_value = ahash(0u128);
         for pos in 0..size {
             let test_value = ahash(0 ^ (1u128 << pos));
-            assert_sufficently_different(compare_value, test_value);
+            assert_sufficiently_different(compare_value, test_value);
         }
     }
 
@@ -53,12 +46,12 @@ mod crypto {
         "test".hash(&mut b);
         "test".hash(&mut c);
         "test".hash(&mut d);
-        assert_sufficently_different(a.finish(), b.finish());
-        assert_sufficently_different(a.finish(), c.finish());
-        assert_sufficently_different(a.finish(), d.finish());
-        assert_sufficently_different(b.finish(), c.finish());
-        assert_sufficently_different(b.finish(), d.finish());
-        assert_sufficently_different(c.finish(), d.finish());
+        assert_sufficiently_different(a.finish(), b.finish());
+        assert_sufficiently_different(a.finish(), c.finish());
+        assert_sufficiently_different(a.finish(), d.finish());
+        assert_sufficiently_different(b.finish(), c.finish());
+        assert_sufficiently_different(b.finish(), d.finish());
+        assert_sufficiently_different(c.finish(), d.finish());
     }
 
     #[test]
@@ -79,27 +72,27 @@ mod crypto {
             "1234".hash(&mut a);
             "1234".hash(&mut b);
             "1234".hash(&mut c);
-            assert_sufficently_different(a.finish(), b.finish());
-            assert_sufficently_different(a.finish(), c.finish());
-            assert_sufficently_different(b.finish(), c.finish());
+            assert_sufficiently_different(a.finish(), b.finish());
+            assert_sufficiently_different(a.finish(), c.finish());
+            assert_sufficiently_different(b.finish(), c.finish());
             let mut a = AHasher::new_with_keys(0, 0);
             let mut b = AHasher::new_with_keys(0, 1 << bit);
             let mut c = AHasher::new_with_keys(1 << bit, 0);
             "12345678".hash(&mut a);
             "12345678".hash(&mut b);
             "12345678".hash(&mut c);
-            assert_sufficently_different(a.finish(), b.finish());
-            assert_sufficently_different(a.finish(), c.finish());
-            assert_sufficently_different(b.finish(), c.finish());
+            assert_sufficiently_different(a.finish(), b.finish());
+            assert_sufficiently_different(a.finish(), c.finish());
+            assert_sufficiently_different(b.finish(), c.finish());
             let mut a = AHasher::new_with_keys(0, 0);
             let mut b = AHasher::new_with_keys(0, 1 << bit);
             let mut c = AHasher::new_with_keys(1 << bit, 0);
             "1234567812345678".hash(&mut a);
             "1234567812345678".hash(&mut b);
             "1234567812345678".hash(&mut c);
-            assert_sufficently_different(a.finish(), b.finish());
-            assert_sufficently_different(a.finish(), c.finish());
-            assert_sufficently_different(b.finish(), c.finish());
+            assert_sufficiently_different(a.finish(), b.finish());
+            assert_sufficiently_different(a.finish(), c.finish());
+            assert_sufficiently_different(b.finish(), c.finish());
         }
     }
 
@@ -111,16 +104,41 @@ mod crypto {
                 string.hash(&mut short);
                 let value = short.finish();
                 let mut string = string.to_string();
-                for _ in 0..128 {
+                for num in 1..=128 {
                     let mut long = AHasher::default();
                     string.push(c as char);
                     string.hash(&mut long);
+                    let same_bytes = count_same_bytes(value, long.finish());
+                    assert!(same_bytes <= 2, format!("{} bytes of {} -> {:x} vs {:x}", num, c, value, long.finish()));
                     let flipped_bits = (value ^ long.finish()).count_ones();
                     assert!(flipped_bits > 10);
                 }
             }
         }
     }
+}
+
+fn assert_sufficiently_different(a: u64, b: u64) {
+    let same_byte_count = count_same_bytes(a, b);
+    assert!(same_byte_count <= 1);
+    let flipped_bits = (a ^ b).count_ones();
+    assert!(flipped_bits > 18 && flipped_bits < 48, "{:x} and {:x}: {:}", a, b, flipped_bits);
+    for rotate in 0..64 {
+        let flipped_bits2 = (a ^ (b.rotate_left(rotate))).count_ones();
+        assert!(flipped_bits2 > 14 && flipped_bits2 < 50, "{:x} and {:x}: {:}", a, b.rotate_left(rotate), flipped_bits2);
+    }
+}
+
+fn count_same_bytes(a: u64, b: u64) -> i32 {
+    let mut same_byte_count = 0;
+    for byte in 0..8 {
+        let ba = a >> 8 * byte as u8;
+        let bb = b >> 8 * byte as u8;
+        if ba == bb {
+            same_byte_count += 1;
+        }
+    }
+    same_byte_count
 }
 
 
@@ -130,18 +148,9 @@ mod fallback {
     use ahash::fallback::FallbackHasher;
 
     pub fn fallback<H: Hash>(b: H) -> u64 {
-        let mut hasher = FallbackHasher::new_with_keys(0, 0);
+        let mut hasher = FallbackHasher::new_with_keys(BAD_KEY, BAD_KEY);
         b.hash(&mut hasher);
         hasher.finish()
-    }
-
-    fn assert_sufficently_different(a: u64, b: u64) {
-        let flipped_bits = (a ^ b).count_ones();
-        assert!(flipped_bits > 14 && flipped_bits < 50, "{:x} and {:x}: {:}", a, b, flipped_bits);
-        for rotate in 0..64 {
-            let flipped_bits = (a ^ (b.rotate_left(rotate))).count_ones();
-            assert!(flipped_bits > 10 && flipped_bits < 54, "{:x} and {:x}: {:x}", a, b, flipped_bits);
-        }
     }
 
     #[test]
@@ -150,19 +159,19 @@ mod fallback {
         let compare_value = fallback(0u32);
         for pos in 0..size {
             let test_value = fallback(0 ^ (1u32 << pos));
-            assert_sufficently_different(compare_value, test_value);
+            assert_sufficiently_different(compare_value, test_value);
         }
         let size = 64;
         let compare_value = fallback(0u64);
         for pos in 0..size {
             let test_value = fallback(0 ^ (1u64 << pos));
-            assert_sufficently_different(compare_value, test_value);
+            assert_sufficiently_different(compare_value, test_value);
         }
         let size = 128;
         let compare_value = fallback(0u128);
         for pos in 0..size {
             let test_value = fallback(0 ^ (1u128 << pos));
-            assert_sufficently_different(compare_value, test_value);
+            assert_sufficiently_different(compare_value, test_value);
         }
     }
 
@@ -176,12 +185,12 @@ mod fallback {
         "test".hash(&mut b);
         "test".hash(&mut c);
         "test".hash(&mut d);
-        assert_sufficently_different(a.finish(), b.finish());
-        assert_sufficently_different(a.finish(), c.finish());
-        assert_sufficently_different(a.finish(), d.finish());
-        assert_sufficently_different(b.finish(), c.finish());
-        assert_sufficently_different(b.finish(), d.finish());
-        assert_sufficently_different(c.finish(), d.finish());
+        assert_sufficiently_different(a.finish(), b.finish());
+        assert_sufficiently_different(a.finish(), c.finish());
+        assert_sufficiently_different(a.finish(), d.finish());
+        assert_sufficiently_different(b.finish(), c.finish());
+        assert_sufficiently_different(b.finish(), d.finish());
+        assert_sufficiently_different(c.finish(), d.finish());
     }
 
     #[test]
@@ -202,27 +211,27 @@ mod fallback {
             "1234".hash(&mut a);
             "1234".hash(&mut b);
             "1234".hash(&mut c);
-            assert_sufficently_different(a.finish(), b.finish());
-            assert_sufficently_different(a.finish(), c.finish());
-            assert_sufficently_different(b.finish(), c.finish());
+            assert_sufficiently_different(a.finish(), b.finish());
+            assert_sufficiently_different(a.finish(), c.finish());
+            assert_sufficiently_different(b.finish(), c.finish());
             let mut a = FallbackHasher::new_with_keys(0, 0);
             let mut b = FallbackHasher::new_with_keys(0, 1 << bit);
             let mut c = FallbackHasher::new_with_keys(1 << bit, 0);
             "12345678".hash(&mut a);
             "12345678".hash(&mut b);
             "12345678".hash(&mut c);
-            assert_sufficently_different(a.finish(), b.finish());
-            assert_sufficently_different(a.finish(), c.finish());
-            assert_sufficently_different(b.finish(), c.finish());
+            assert_sufficiently_different(a.finish(), b.finish());
+            assert_sufficiently_different(a.finish(), c.finish());
+            assert_sufficiently_different(b.finish(), c.finish());
             let mut a = FallbackHasher::new_with_keys(0, 0);
             let mut b = FallbackHasher::new_with_keys(0, 1 << bit);
             let mut c = FallbackHasher::new_with_keys(1 << bit, 0);
             "1234567812345678".hash(&mut a);
             "1234567812345678".hash(&mut b);
             "1234567812345678".hash(&mut c);
-            assert_sufficently_different(a.finish(), b.finish());
-            assert_sufficently_different(a.finish(), c.finish());
-            assert_sufficently_different(b.finish(), c.finish());
+            assert_sufficiently_different(a.finish(), b.finish());
+            assert_sufficiently_different(a.finish(), c.finish());
+            assert_sufficiently_different(b.finish(), c.finish());
         }
     }
 
@@ -230,14 +239,16 @@ mod fallback {
     fn test_padding_doesnot_collide() {
         for c in 0..128u8 {
             for string in ["", "1234", "12345678", "1234567812345678"].into_iter() {
-                let mut short = FallbackHasher::default();
+                let mut short = FallbackHasher::new_with_keys(BAD_KEY, BAD_KEY);
                 string.hash(&mut short);
                 let value = short.finish();
                 let mut string = string.to_string();
-                for _ in 0..128 {
-                    let mut long = FallbackHasher::default();
+                for num in 1..=128 {
+                    let mut long = FallbackHasher::new_with_keys(BAD_KEY, BAD_KEY);
                     string.push(c as char);
                     string.hash(&mut long);
+                    let same_bytes = count_same_bytes(value, long.finish());
+                    assert!(same_bytes <= 2, format!("{} bytes of {} -> {:x} vs {:x}", num, c, value, long.finish()));
                     let flipped_bits = (value ^ long.finish()).count_ones();
                     assert!(flipped_bits > 10,
                             format!("Too few bits fliped {} -> {:x} vs {} -> {:x}", string, value, c, long.finish()));
