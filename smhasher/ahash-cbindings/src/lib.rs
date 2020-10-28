04c2b8d0d5978407ed2c7563f675a6c3f180698d
Tom Kaitchuck <Tom.Kaitchuck@gmail.com>
2020-10-27 20:44:30 -0700

    Update c-bindings to use 4 arg initialization function

--- smhasher/ahash-cbindings/src/lib.rs
+++ smhasher/ahash-cbindings/src/lib.rs
@@ -1,12 +1,17 @@
-use core::slice;
 use ahash::*;
-use std::hash::{Hasher, BuildHasher};
+use core::slice;
+use std::hash::{BuildHasher, Hasher};
 
 #[no_mangle]
 pub extern "C" fn ahash64(buf: *const (), len: usize, seed: u64) -> u64 {
     let buf: &[u8] = unsafe { slice::from_raw_parts(buf as *const u8, len) };
-    let mut hasher = RandomState::with_seeds(std::f64::consts::PI as u64 ^ seed, std::f64::consts::E as u64 ^ seed).build_hasher();
+    let mut hasher = RandomState::with_seeds(
+        seed,
+        std::f64::consts::PI as u64,
+        std::f64::consts::E as u64,
+        std::f64::consts::SQRT_2 as u64,
+    )
+    .build_hasher();
     hasher.write(buf);
     hasher.finish()
 }
-
