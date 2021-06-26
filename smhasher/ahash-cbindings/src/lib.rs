2363bc7d6a335c540632e84c89be6d69db6be422
Tom Kaitchuck <tom.kaitchuck@emc.com>
2021-06-25 17:05:40 -0700

    Update c bindings to support new smhasher.
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- smhasher/ahash-cbindings/src/lib.rs
+++ smhasher/ahash-cbindings/src/lib.rs
@@ -5,13 +5,6 @@ use std::hash::{BuildHasher, Hasher};
 #[no_mangle]
 pub extern "C" fn ahash64(buf: *const (), len: usize, seed: u64) -> u64 {
     let buf: &[u8] = unsafe { slice::from_raw_parts(buf as *const u8, len) };
-    let mut hasher = RandomState::with_seeds(
-        0x243f_6a88_85a3_08d3_u64.wrapping_add(seed),
-        0x1319_8a2e_0370_7344_u64 ^ seed,
-        0xa409_3822_299f_31d0,
-        0x082e_fa98_ec4e_6c89,
-    )
-    .build_hasher();
-    hasher.write(buf);
-    hasher.finish()
+    let build_hasher = RandomState::with_seeds(seed, 0, 0, 0);
+    <[u8]>::get_hash(&buf, &build_hasher)
 }
