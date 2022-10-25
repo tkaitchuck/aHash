2c1a088cee2dd0d077a11d62a02e4ebff78608e3
Tom Kaitchuck <tkaitchuck@users.noreply.github.com>
2022-10-25 13:39:33 -0700

    Run smhasher test with weaker keys

--- smhasher/ahash-cbindings/src/lib.rs
+++ smhasher/ahash-cbindings/src/lib.rs
@@ -7,6 +7,6 @@ use std::hash::{BuildHasher};
 #[no_mangle]
 pub extern "C" fn ahash64(buf: *const (), len: usize, seed: u64) -> u64 {
     let buf: &[u8] = unsafe { slice::from_raw_parts(buf as *const u8, len) };
-    let build_hasher = RandomState::with_seed(seed as usize);
+    let build_hasher = RandomState::with_seeds(seed, seed, seed, seed);
     build_hasher.hash_one(&buf)
 }
