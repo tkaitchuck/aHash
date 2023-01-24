1ca798444b2bcdea862ab721cc790d6542f3fa76
Tom Kaitchuck <tom.kaitchuck@emc.com>
2023-01-24 12:13:05 -0800

    Use faster seeing for c bindings
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- smhasher/ahash-cbindings/src/lib.rs
+++ smhasher/ahash-cbindings/src/lib.rs
@@ -5,6 +5,6 @@ use std::hash::{BuildHasher, Hasher};
 #[no_mangle]
 pub extern "C" fn ahash64(buf: *const (), len: usize, seed: u64) -> u64 {
     let buf: &[u8] = unsafe { slice::from_raw_parts(buf as *const u8, len) };
-    let build_hasher = RandomState::with_seeds(seed, 0, 0, 0);
+    let build_hasher = RandomState::with_seed(seed as usize);
     <[u8]>::get_hash(&buf, &build_hasher)
 }
