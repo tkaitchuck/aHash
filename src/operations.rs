0d1d2a313b036a46d845b4fbdf62bb215b7f17af
Pure White <wudi.daniel@bytedance.com>
2023-10-23 22:33:43 +0800

    fix compile

--- src/operations.rs
+++ src/operations.rs
@@ -123,13 +123,12 @@ pub(crate) fn aesenc(value: u128, xor: u128) -> u128 {
     use core::arch::aarch64::*;
     #[cfg(target_arch = "arm")]
     use core::arch::arm::*;
-    use core::mem::transmute;
+
     unsafe {
         let value = transmute!(value);
-    let value = transmute!(value);
-    let res = unsafe { vaesmcq_u8(vaeseq_u8(value, transmute!(0u128))) };
-    let value: u128 = transmute!(res);
-    xor ^ value
+        let res = unsafe { vaesmcq_u8(vaeseq_u8(value, transmute!(0u128))) };
+        let value: u128 = transmute!(res);
+        xor ^ value
     }
 }
 
@@ -164,7 +163,6 @@ pub(crate) fn aesdec(value: u128, xor: u128) -> u128 {
     let res = unsafe { vaesimcq_u8(vaesdq_u8(value, transmute!(0u128))) };
     let value: u128 = transmute!(res);
     xor ^ value
-    }
 }
 
 #[allow(unused)]
