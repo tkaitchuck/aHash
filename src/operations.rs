7bbbd833cb48c6f8e64bd94c196a700c09bb97e4
Pure White <wu.purewhite@gmail.com>
2023-10-24 02:29:56 +0800

    fix: #170 compile fail on aarch64 (#171)
    compile fail on aarch64
    
    Co-authored-by: Joshua Liebow-Feeser <joshlf@users.noreply.github.com>
    Co-authored-by: Joshua Liebow-Feeser <joshlf@users.noreply.github.com>
    Co-authored-by: Tom Kaitchuck <tkaitchuck@users.noreply.github.com>

--- src/operations.rs
+++ src/operations.rs
@@ -123,10 +123,9 @@ pub(crate) fn aesenc(value: u128, xor: u128) -> u128 {
     use core::arch::aarch64::*;
     #[cfg(target_arch = "arm")]
     use core::arch::arm::*;
-    unsafe {
-        let value = transmute!(value);
-        xor ^ transmute::<_, u128>(vaesmcq_u8(vaeseq_u8(value, transmute!(0u128))))
-    }
+    let res = unsafe { vaesmcq_u8(vaeseq_u8(transmute!(value), transmute!(0u128))) };
+    let value: u128 = transmute!(res);
+    xor ^ value
 }
 
 #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "aes", not(miri)))]
@@ -156,10 +155,9 @@ pub(crate) fn aesdec(value: u128, xor: u128) -> u128 {
     use core::arch::aarch64::*;
     #[cfg(target_arch = "arm")]
     use core::arch::arm::*;
-    unsafe {
-        let value = transmute!(value);
-        xor ^ transmute::<_, u128>(vaesimcq_u8(vaesdq_u8(value, transmute!(0u128))))
-    }
+    let res = unsafe { vaesimcq_u8(vaesdq_u8(transmute!(value), transmute!(0u128))) };
+    let value: u128 = transmute!(res);
+    xor ^ value
 }
 
 #[allow(unused)]
