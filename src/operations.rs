95eb4f47109189dff2e59d18e24e789976bd6c1b
Tom Kaitchuck <tkaitchuck@users.noreply.github.com>
2024-02-19 17:24:35 -0800

    Issue #207: Rollback MSRV bump (#208)
    Signed-off-by: Tom Kaitchuck <Tom.Kaitchuck@gmail.com>

--- src/operations.rs
+++ src/operations.rs
@@ -1,4 +1,5 @@
 use crate::convert::*;
+#[allow(unused)]
 use zerocopy::transmute;
 
 ///This constant comes from Kunth's prng (Empirically it works better than those from splitmix32).
@@ -111,7 +112,7 @@ pub(crate) fn aesenc(value: u128, xor: u128) -> u128 {
 }
 
 #[cfg(any(
-    all(target_arch = "aarch64", target_feature = "aes", not(miri)),
+    all(feature = "nightly-arm-aes", target_arch = "aarch64", target_feature = "aes", not(miri)),
     all(feature = "nightly-arm-aes", target_arch = "arm", target_feature = "aes", not(miri)),
 ))]
 #[allow(unused)]
@@ -141,7 +142,7 @@ pub(crate) fn aesdec(value: u128, xor: u128) -> u128 {
 }
 
 #[cfg(any(
-    all(target_arch = "aarch64", target_feature = "aes", not(miri)),
+    all(feature = "nightly-arm-aes", target_arch = "aarch64", target_feature = "aes", not(miri)),
     all(feature = "nightly-arm-aes", target_arch = "arm", target_feature = "aes", not(miri)),
 ))]
 #[allow(unused)]
--- src/random_state.rs
+++ src/random_state.rs
@@ -2,7 +2,7 @@ use core::hash::Hash;
 cfg_if::cfg_if! {
     if #[cfg(any(
         all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "aes", not(miri)),
-        all(target_arch = "aarch64", target_feature = "aes", not(miri)),
+        all(feature = "nightly-arm-aes", target_arch = "aarch64", target_feature = "aes", not(miri)),
         all(feature = "nightly-arm-aes", target_arch = "arm", target_feature = "aes", not(miri)),
     ))] {
         use crate::aes_hash::*;
