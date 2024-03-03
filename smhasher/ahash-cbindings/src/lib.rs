7dc83fc43afe5b6a95a85bce7f946e19bfb88cd8
Tom Kaitchuck <Tom.Kaitchuck@gmail.com>
2024-03-02 18:16:32 -0800

    All smhasher tests pass
    Signed-off-by: Tom Kaitchuck <Tom.Kaitchuck@gmail.com>

--- smhasher/ahash-cbindings/src/lib.rs
+++ smhasher/ahash-cbindings/src/lib.rs
@@ -5,6 +5,6 @@ use std::hash::{BuildHasher};
 #[no_mangle]
 pub extern "C" fn ahash64(buf: *const (), len: usize, seed: u64) -> u64 {
     let buf: &[u8] = unsafe { slice::from_raw_parts(buf as *const u8, len) };
-    let build_hasher = RandomState::with_seeds(seed, seed, seed, seed);
+    let build_hasher = RandomState::<&[u8]>::with_seeds(seed, seed, seed, seed);
     build_hasher.hash_one(&buf)
 }
